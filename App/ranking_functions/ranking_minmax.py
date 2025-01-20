947226291b1ed0de13a809a95f3ae9e57ebb788d
daydunedortmans <76011815+daydunedortmans@users.noreply.github.com>
2025-01-20 11:51:21 +0100

    ranking change (evaluation) + requirements add

--- App/ranking_functions/ranking_minmax.py
+++ App/ranking_functions/ranking_minmax.py
@@ -52,4 +52,4 @@ def get_min_max_ranking(dmatrix, df):
     partnership_df['Rank'] = partnership_df.index + 1
 
     # Reorder columns for clarity
-    return partnership_df[['Rank', 'Company A', 'Company B']]
+    return partnership_df[['Rank', 'Company A', 'Company B', 'Min_Max_Score']]
